9885f686273c55435f45eb29dba7cdd013fd28c4
rusu55 <rusu55@yahoo.com>
2024-04-03 18:33:10 -0500

    updated

--- components/services/AdditionalServices.tsx
+++ components/services/AdditionalServices.tsx
@@ -10,11 +10,11 @@ export const AdditionalServices = () => {
         </div>
         <div className='w-full md:w-[60%]'>
             <div>
-              <h5 className='text-lg  pt-4 text-left  border-b border-dashed border-slate-200'>Event Photography <span className="text-roze float-right text-sm">min 4 Hours Coverage</span></h5>
-              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Second photographer <span className="text-roze float-right text-sm">6 Hours to 10 Hours Coverage</span></h5>
-              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Second Cinematographer <span className="text-roze float-right text-sm">6 Hours to 10 Hours Coverage</span></h5>
-              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Extended coverage <span className="text-roze float-right text-sm">min 1 Hour</span></h5>
-              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Engagement or Family Sessin <span className="text-roze float-right text-sm">45 Min Coverage</span></h5>
+              <h5 className='text-lg  pt-4 text-left  border-b border-dashed border-slate-200'>Event Photography <span className="text-roze float-right text-sm">min 4h Coverage</span></h5>
+              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Second Photographer <span className="text-roze float-right text-sm">6h to 10h Coverage</span></h5>
+              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Second Videographer <span className="text-roze float-right text-sm">6h to 10h Coverage</span></h5>
+              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Extended Coverage <span className="text-roze float-right text-sm">min 1h</span></h5>
+              <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Engagement or Family Sessin <span className="text-roze float-right text-sm">45min Coverage</span></h5>
               <h5 className='text-lg pt-6 text-left  border-b border-dashed border-slate-200'>Drone Coverage <span className="text-roze float-right text-sm">contact us for details</span></h5>
             </div>
         </div>
