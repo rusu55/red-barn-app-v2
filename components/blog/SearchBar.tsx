ccf5749c3b4176526c083acf6faefe3f982964c0
rusu55 <rusu55@yahoo.com>
2024-04-03 13:14:06 -0500

    updated

--- components/blog/SearchBar.tsx
+++ components/blog/SearchBar.tsx
@@ -29,10 +29,10 @@ export const SearchBar = ({ search }: { search?: string }) => {
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"></div>
       <input
         value={text}
-        placeholder="Search movies..."
+        placeholder="Search blogs..."
         onChange={(e) => setText(e.target.value)}
-        className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-      />
+        className="block w-full rounded-md border-0 py-1.5 pl-10 ring-1 ring-inset ring-gray-300 placeholder:text-grey-300 focus:ring-roze focus:ring-inset focus:ring-2 focus:outline-none sm:text-sm sm:leading-6 transition-all duration-300"
+      />  
     </div>
   );
 };
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -11,13 +11,13 @@ const Footer = () => {
     <footer ref={containerRef}>
       <div className="bg-neutral-100/75">
         <div className="mx-auto max-w-screen-xl">
-          <div className="flex flex-col items-center space-y-12 md:flex-row md:justify-evenly py-20">
+          <div className="flex flex-col items-start space-y-12 md:flex-row md:justify-evenly md:space-y-0 py-20">
             <div>
               <h6 className="relative text-lg font-semibold tracking-wide text-black">
                 <span className="relative z-20">Connect with Us</span>
                 <span className="absolute -bottom-1 left-0 z-10 h-[1px] w-full rounded-lg bg-neutral-300" />
               </h6>
-              <ul className="mt-6 text-lg space-y-2 text-center md:text-right">
+              <ul className="pt-6 text-lg space-y-2 text-center md:text-right">
                 <li className="font-medium text-xs text-neutral-400 duration-300 ease-in-out hover:text-roze">
                   <Link href="https://www.facebook.com/RedBarnWeddingStudio">FACEBOOK</Link>
                 </li>
@@ -47,7 +47,7 @@ const Footer = () => {
                 <span className="absolute -bottom-1 left-0 z-10 h-[1px] w-full rounded-lg bg-neutral-300" />
               </h6>
 
-              <ul className="mt-6 text-lg space-y-2 text-center md:text-right">
+              <ul className="pt-6 text-lg space-y-2 text-center md:text-right">
                 <li className="font-medium text-xs text-neutral-400 duration-300 ease-in-out hover:text-roze">
                   <Link href="/">HOME</Link>
                 </li>
