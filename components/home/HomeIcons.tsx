2b460b2c6e02264dd37c4f5edaf4353a135d3252
rusu55 <rusu55@yahoo.com>
2024-04-09 15:44:21 -0500

    updated

--- components/home/HomeIcons.tsx
+++ components/home/HomeIcons.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 import tk from '/public/images/tk_badge.png';
 import ww from '/public/images/weddingwire_badge.png';
 import hf from '/public/images/tk_halloffame.png'
@@ -8,15 +9,15 @@ export const HomeIcons = () => {
   return (
     <section className="my-16">
         <div className="mx-auto max-w-screen-xl flex flex-wrap justify-center">
-            <div className='w-1/3'>
+            <div className='w-full px-4 md:w-1/3'>
                 <h2>Check our reviews on:</h2>
                 <hr className="line line-hr-center"></hr>
-                <p className='w-[80%] pt-5'>Because every picture should be unique. Making your special day. We will make a wonderful story</p>
+                <p className='w-full md:w-[80%] pt-5'>Because every picture should be unique. Making your special day. We will make a wonderful story</p>
             </div>
             <div className='flex items-center gap-8'>
-                    <Image src={tk} alt="" width={120}/>
-                    <Image src={ww} alt="" width={110}/>
-                    <Image src={hf} alt="" width={120}/>
+                    <Link href="https://www.theknot.com/marketplace/red-barn-wedding-studio-schaumburg-il-2027022"><Image src={tk} alt="" className='w-20 md:w-28 object-cover' /></Link>
+                    <Link href="https://www.weddingwire.com/biz/red-barn-wedding-studio/776d90b9b48065a1.html"><Image src={ww} alt="" className='w-20 md:w-28 object-cover'/></Link>
+                    <Link href="https://www.theknot.com/marketplace/red-barn-wedding-studio-schaumburg-il-2027022"><Image src={hf} alt="" className='w-20 md:w-28 object-cover'/></Link>
              </div>
            
         </div>
