23eab03ec8bca0e66d865207dcf0cfe16c56d6e6
rusu55 <rusu55@yahoo.com>
2024-04-10 15:46:06 -0500

    updated

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -11,10 +11,10 @@ export const Hero = ({image, title, description} : Props) => {
   return (
     <div className='w-full bg-center bg-cover' style={{backgroundImage: `url(${image.src})`}}>
       <div className='flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-10 py-8 md:py-32'>
-        <div className='w-[80%] md:max-w-lg bg-white/90 md:bg-white py-9 px-7 text-center '>
-            <span className='font-normal font-pariss text-xl'>Red Barn Wedding Studio</span>
+        <div className='w-[60%] md:max-w-lg bg-white/90 md:bg-white py-4 md:py-9 px-7 text-center '>
+            <span className='font-normal font-pariss text-lg md:text-xl'>Red Barn Wedding Studio</span>
             <hr className="line line-hr-center mx-auto" />
-            <h1 className='text-2xl md:text-4xl pt-4'>Wedding Photography Cinematography</h1>
+            <h1 className='text-base md:text-4xl pt-2 md:pt-4'>Wedding Photography Cinematography</h1>
         </div>          
       </div>
     </div>
