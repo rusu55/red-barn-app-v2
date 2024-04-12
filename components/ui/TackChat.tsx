fa612c8ce099c729c71711843623a7ebcc65244d
rusu55 <rusu55@yahoo.com>
2024-04-12 14:33:56 -0500

    updated

--- components/ui/TackChat.tsx
+++ components/ui/TackChat.tsx
@@ -9,8 +9,7 @@ const handleMinimize = () => {
     tawkMessengerRef.current.minimize();
 };
   return (
-    <>
-    <button onClick={handleMinimize}> Minimize the Chat </button>
+    <>    
     <TawkMessengerReact
                 propertyId="65deb1a68d261e1b5f664ad8"
                 widgetId="1hnmubs9f"
