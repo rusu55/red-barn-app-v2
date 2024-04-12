4b62a97d655e7f14a1758c977fb50891cef49862
rusu55 <rusu55@yahoo.com>
2024-04-12 09:36:45 -0500

    updated

--- app/services/wedding_photobooth/page.tsx
+++ app/services/wedding_photobooth/page.tsx
@@ -3,6 +3,7 @@ import { Hero } from "@/components/ui/Hero";
 import { StripsSlider } from "@/components/services/StripsSlider";
 import Img from "/public/images/photography-hero.jpg";
 import photobooth from "/public/images/photobooth.jpg";
+import { Button } from "@/components/ui/Button";
 
 const PhotoboothPage = () => {
   return (
@@ -17,11 +18,11 @@ const PhotoboothPage = () => {
             photobooth is the top shelf of booths.
           </p>
         </div>
-        <div className="flex flex-wrap items-start justify-center mt-10">
+        <div className="flex flex-wrap items-start justify-center pt-10">
           {/* Left Column */}
           <div className="order-1 w-full md:w-1/4">
-            <div className="w-full flex flex-col items-center md:items-end ">
-              <h2 className=" !text-2xl md:!text-3xl">Quality Photobooth</h2>
+            <div className="w-full flex flex-col items-center md:items-end">
+              <h2 className=" !text-2xl md:!text-3xl duration-500 ease-in-out hover:scale-105 cursor-pointer">Quality Photobooth</h2>
               <p className="pt-4 text-center md:text-right ">
                 Quality built booths for worry free events! <br />
                 Our photobooth is a open booth that can accommodate 10
@@ -30,56 +31,50 @@ const PhotoboothPage = () => {
               <span className="border-t border-roze w-[120px]" />
             </div>
             <div className="pt-8 w-full flex flex-col items-center md:items-end">
-              <h2 className=" !text-2xl md:!text-3xl">Live Printing</h2>
+              <h2 className=" !text-2xl md:!text-3xl duration-500 ease-in-out hover:scale-105 cursor-pointer">Live Printing</h2>
               <p className="pt-4 text-center md:text-right">
-                Super gloss paper, printed in seconds.
+                 Unlimited prints are included in the rental price 
                 <br />2 strips of 2×6 at a time
               </p>
               <span className="border-t border-roze w-[120px]" />
             </div>
             <div className="pt-8 w-full flex flex-col items-center md:items-end">
-              <h2 className="!text-2xl md:!text-3xl">Online Gallery</h2>
-              <p className="pt-4 text-center md:text-right">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus consequuntur ad expedita perferendis</p>
+              <h2 className="!text-2xl md:!text-3xl duration-500 ease-in-out hover:scale-105 cursor-pointer">Online Gallery</h2>
+              <p className="pt-4 text-center md:text-right">Online Gallery with all the Sztrips to share with friends and family</p>
               <span className="border-t border-roze w-[120px]" />
             </div>
           </div>
           <div className="pt-8 md:pt-0 order-3 md:order-2 w-full md:w-1/2">
-            <Image src={photobooth} alt="" className="w-[200px] md:w-[300px] object-cover mx-auto" />
+            <Image src={photobooth} alt="" className="w-[200px] md:w-[300px] object-cover mx-auto duration-500 ease-in-out hover:scale-105 cursor-pointer" />
           </div>
           {/* Right Column*/}
           <div className="order-2 md:order-3 w-full md:w-1/4">
-            <div className="pt-8 md:pt-0 w-full flex flex-col items-center md:items-start">
-              <h2 className="!text-2xl md:!text-3xl">Unlimited Prints</h2>
-              <p className="pt-4 text-center md:text-left">
-                Unlimited prints are included in the rental price so each person
-                in the photo gets a print.
-                <br />
-                Take pictures 5 times or 10 times (does not matter to us)
-              </p>
-              <span className="border-t border-roze w-[120px]" />
-            </div>
-            <div className="pt-8 w-full flex flex-col items-center md:items-start">
-              <h2 className="!text-2xl md:!text-3xl">Unlimited Props</h2>
+            
+            <div className="pt-8 w-full flex flex-col items-center md:items-start md:pt-0">
+              <h2 className="!text-2xl md:!text-3xl duration-500 ease-in-out hover:scale-105 cursor-pointer">Unlimited Props</h2>
               <p className="pt-4 text-center  md:text-left">
-                we have everything from wigs, hats, moustaches, fancy glasses
+                We have everything from wigs, hats, moustaches, fancy glasses
                 and others...
               </p>
               <span className="border-t border-roze w-[120px]" />
             </div>
             <div className="pt-8 w-full flex flex-col items-center md:items-start">
-              <h2 className="!text-2xl md:!text-3xl">Scrapbook Included</h2>
+              <h2 className="!text-2xl md:!text-3xl duration-500 ease-in-out hover:scale-105 cursor-pointer">Scrapbook Included</h2>
               <p className="pt-4 text-center  md:text-left">
-                1 scrapbook so you can collect all the memories and have a
+                1 Scrapbook so you can collect all the memories and have a
                 wonderful keepsake
               </p>
               <span className="border-t border-roze w-[120px]" />
             </div>
             <div className="pt-8 w-full flex flex-col items-center md:items-start">
-              <h2 className="!text-2xl md:!text-3xl">Choice of backdrop</h2>
-              <p className="pt-4 text-center  md:text-left">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <h2 className="!text-2xl md:!text-3xl duration-500 ease-in-out hover:scale-105 cursor-pointer">Choice of backdrop</h2>
+              <p className="pt-4 text-center  md:text-left">Your choice out of deferent backdrops colors to mactch your Wedding Theme</p>
               <span className="border-t border-roze w-[120px]" />
             </div>
-          </div>
+          </div>          
+        </div>
+        <div className="w-full pt-10 flex justify-center">
+            <Button href="/contact">Contact Us</Button>
         </div>
         {/* Slider */}
         <StripsSlider />
--- components/contact/ContactIntro.tsx
+++ components/contact/ContactIntro.tsx
@@ -13,13 +13,13 @@ export const ContactIntro = () => {
   
   return (
     <div>
-      <div>
-        <span>Contact Us</span>
-        <h2>I`d love to tell your story.</h2>
+      <div className="w-full flex flex-col items-center">
+        <span className="uppercase text-roze text-sm tracking-widest">Contact Us</span>
+        <h2 className="w-[80%] text-center py-4">I`d love to tell your story.</h2>
       </div>
       <div className="w-full">
         <p>We are excited to go wherever your love story is taking me and looking forward to hearing from you!</p>
-        <div>
+        <div className="pt-6">
              <ul className="mx-auto w-[80%] ">
                 <li className="py-2 border-b-2 border-roze flex justify-between items-center"><span className=" uppercase">Phone:</span><span className="font-pariss text-lg text-right w-[80%] leading-9">(312) 912-5004</span></li>
                 <li className="py-2 border-b-2 border-roze flex justify-between items-center"><span className=" uppercase">Email:</span><span className="font-pariss text-lg text-right w-[80%] leading-9">RedBarnBride@yahoo.com</span></li> 
@@ -31,9 +31,8 @@ export const ContactIntro = () => {
           <Button href="https://redbarnweddingstudio.setmore.com">Book An Appoiment</Button>
         </div>
       </div>
-      <div className="w-full px-4 md:w-[38%] flex flex-col gap-y-6"></div>
-      <div className="mt-10">
-        <h2>Follow US on: </h2>
+      <div className="w-full px-4 flex flex-col items-center justify-center gap-y-6">      
+        <h2 className="pt-4">Follow US on: </h2>
         <div className="flex items-center justify-start gap-x-2 cursor-pointer">
         <RevealComponent index={1} variants={fadeTopBottom}>
             <Link href="https://www.facebook.com/RedBarnWeddingStudio"><Icon icon="facebook" className="text-roze w-12 h-12"/></Link>
@@ -48,6 +47,7 @@ export const ContactIntro = () => {
             <Link href="https://www.youtube.com/channel/UCnTzNTKip1rjTTzd5vqO5HA"><Icon icon="youtube" className="text-roze  w-12 h-12"/></Link>
           </RevealComponent>
         </div>
+      
       </div>
     </div>
   );
