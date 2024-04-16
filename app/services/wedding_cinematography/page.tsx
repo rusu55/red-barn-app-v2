b76e86f700284317cb6d29490a5e6545c5bb7e76
rusu55 <rusu55@outlook.com>
2024-04-16 10:01:13 -0500

    updated

--- app/services/wedding_cinematography/page.tsx
+++ app/services/wedding_cinematography/page.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
 
 const CinematographyPage = () => {
   return (
-    <div>CinematographyPage</div>
-  )
-}
+    <div>
+      <div>
+        When it comes to choosing the right package for your wedding day – we do
+        our best to make the selection process as easy as possible. We offer
+        three categories of services: Wedding Videography, Wedding Photography,
+        and a combination of both. Each category has three general packages that
+        are designed in a way to provide you with the best experience possible.
+        All of the packages are hourly based and differ in the amount of footage
+        delivered. In summary, whether you choose the least or most advanced
+        package, we will create memories that you remember forever.
+      </div>
+      <div>
+        Our videography packages are focused on delivering a cinematic wedding
+        film that tells a story of your special day. The films are 3-6 minutes
+        long and will be delivered between 4-6 weeks after the wedding. Length
+        of the film depends on the size of your wedding, number of hours you’ve
+        hired us for, and the amount of events occurring through the day. For
+        the addition features, please see the adds on section below the
+        packages. All of the packages can be customized at any time.
+      </div>
+      <div>
+        Your wedding went amazingly and you can’t wait to share your wonderful
+        memories with the rest of your family and friends, we’ve got you
+        covered! Our wedding teaser is a 1-minute video that includes the most
+        memorable and epic moments of your BIG DAY! The teaser is edited with
+        the song of your choice and delivered only 3 days after your wedding.
+        It’s a perfect express option for couples who are willing to share their
+        wedding highlights on social platforms such as Facebook, Twitter, and
+        Instagram. #wegotmarried
+      </div>
+      <div>
+        Had a lot of good memories? Great! Let us edit them all in our special
+        Full-Length Wedding Film – 45 to 120 min documentary-style film of the
+        entire wedding day. All video files are polished and put together in
+        chronological order, hosting a variety of songs and voice-overs. The
+        edit will include the process of getting ready, couples first look, the
+        ceremony, speeches with voice-overs, and lots of dances, along with all
+        your included traditions. Say no more, get comfy and enjoy the viewing!
+      </div>
+      <div>
+        An engagement photoshoot is a great way for a couple to meet their
+        future wedding photographers. We believe that all couples are different
+        and require a specific approach in order to get the best footage
+        possible. Through the photoshoot, we learn more about you as a couple,
+        your love story, stylistic preferences, and simply prepare you to be
+        comfortable in front of the camera. Moreover, it allows us to be as
+        creative as possible without worrying about timing and other factors
+        throughout the wedding day. Our engagement shoots usually take around
+        2-3 hours and take place in parks, museums, beaches, botanic gardens,
+        deserts, mountains and other iconic places in the Los Angeles area. The
+        locations we choose from are Malibu Coast, Venice, Beverly Hills, DTLA,
+        Bel Air, San Pedro, Joshua Tree, Big Bear mountain and many more.
+        Lastly, we want to let you know that we are always open to new
+        suggestions and will do our best to shoot at your preferred location!
+      </div>
+      <div>
+        Weddings can often come in large sizes, as such, a second videographer
+        or photographer can come in handy. They are a significant help to the
+        lead shooter and make the entire filming process easier and faster for
+        the couple. The second shooter also doubles the amount of footage,
+        making sure every priceless moment has been captured.
+      </div>
+      <div>
+        Having shot over 100 weddings, we’ve noticed that weddings have a
+        tendency to run a little late, and there are cases where a few extra
+        hours wouldn’t hurt. Sometimes weddings may have 2 or even 3 locations
+        throughout the day and transit time between them may be significant,
+        especially in Los Angeles. The extra time will ensure that the shooting
+        team has enough time to properly set up for the important moments, plan
+        a specific shot, organize the crowd for group photos, and perform in the
+        most efficient way.
+      </div>
+    </div>
+  );
+};
 
-export default CinematographyPage
+export default CinematographyPage;
