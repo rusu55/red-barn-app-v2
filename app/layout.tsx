9907f3db74ce0f52252e394e2c49503bd699fac4
rusu55 <rusu55@outlook.com>
2024-04-16 20:34:51 -0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,12 +28,6 @@ export const metadata: Metadata = {
   title: "Chicago Luxury Wedding Photography and Vinematography",
   description:
     "Book your Wedding Day with Red Barn Wedding Studio, the best of wedding photography and cinematography in Chicago. Cherish memories for a lifetime.",
-  openGraph: {
-    title: "Title webtsite",
-    description: "this is the desciption",
-    images:
-      "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/5acd99b1-8561-4ef0-84ee-6bdea535dd9b.jpg",
-  },
 };
 
 export default function RootLayout({
