9220094d607deb8a3a110b17cb07bd503b6a36c3
rusu55 <rusu55@outlook.com>
2024-04-16 20:15:30 -0500

    updated

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -11,7 +11,10 @@ export default async function og() {
   return new ImageResponse(
     (
       <div tw="relative flex items-center justify-center">
-        <img src="/images/redbarn.jpg" alt="Red Barn Wedding Studio" />
+        <img
+          src="https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/5acd99b1-8561-4ef0-84ee-6bdea535dd9b.jpg"
+          alt="Red Barn Wedding Studio"
+        />
         <div tw="absolute flex bg-black opacity-50 inset-0" />
         <div tw="absolute flex items-center top-2 w-full">
           <p tw="text-white text-4xl flex  font-bold m-5">
