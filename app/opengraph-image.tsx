2121fe54c1ab4ba43aefe9f8c97f219646e46c56
rusu55 <rusu55@outlook.com>
2024-04-16 20:10:42 -0500

    updated

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,24 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 900,
+  height: 450,
+};
+
+export const contentType = "image/png";
+
+export default async function og() {
+  return new ImageResponse(
+    (
+      <div tw="relative flex items-center justify-center">
+        <img src="/images/redbarn.jpg" alt="Red Barn Wedding Studio" />
+        <div tw="absolute flex bg-black opacity-50 inset-0" />
+        <div tw="absolute flex items-center top-2 w-full">
+          <p tw="text-white text-4xl flex  font-bold m-5">
+            Red Barn Wedding Studio
+          </p>
+        </div>
+      </div>
+    )
+  );
+}
