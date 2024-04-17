8b10aba6ed1b28fb68f6ac86fe3821cc3b81d3df
rusu55 <rusu55@yahoo.com>
2024-04-16 19:36:31 -0500

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Hero } from "@/components/ui/Hero";
 import { AboutIntro } from "@/components/about/AboutIntro";
 import HomeReviews from "@/components/home/HomeReviews";
@@ -5,6 +6,11 @@ import { HomeIcons } from "@/components/home/HomeIcons";
 
 import aboutImage from "/public/images/about-hero.jpg";
 import img1 from '/public/images/alexis.jpg'
+
+export const metadata: Metadata = {
+  title: "Chicago Wedding Photographer - Red Barn Wedding Studio",
+  description: "Voted the Best Chicago Wedding Photographer  and Best of Weddings by The Knot. With over 10 years of experience, we understand that your wedding is a celebration for two of you and the people coming together."
+}
 
 const About = () => {
   return (
