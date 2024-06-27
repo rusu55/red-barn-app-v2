56d21cb7078118e07d69e00c1b626df7fbd6b893
rusu55 <rusu55@yahoo.com>
2024-06-26 21:50:35 -0500

    updateed

--- app/(utils)/video-questionnaire/components/video-form.tsx
+++ app/(utils)/video-questionnaire/components/video-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {z} from 'zod';
 import { Field, Fieldset, Input, Label, Legend, Select, Textarea, Button } from '@headlessui/react';
@@ -38,12 +38,17 @@ const VideoForm = ({onSubmit, disabled}: Props) => {
       <Legend className="text-base/7 font-semibold">Shipping details</Legend>
       <Field>
           <Label className="text-sm/6 font-medium ">Street address</Label>
-          <Input
-            className={clsx(
-              'mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black',
-              'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-            )}
-          />
+          <Controller
+           name="firstName"
+           control={control}
+           render={({ field }) =><Input {...field}
+           className={clsx(
+             'mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black',
+             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+           )}
+         />}
+      />
+          
         </Field>
         <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white" type="submit">
       Save changes
