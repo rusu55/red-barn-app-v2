ac7c32976211923e420cf95bc262f01ce4e5979b
rusu55 <rusu55@outlook.com>
2024-06-27 22:46:33 -0500

    updated

--- app/(utils)/video-questionnaire/page.tsx
+++ app/(utils)/video-questionnaire/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import dynamic from "next/dynamic";
 import { toast } from "react-hot-toast";
@@ -11,28 +12,28 @@ import aboutImage from "/public/images/about-hero.jpg";
 
 const VideoQuestionnaire = () => {
   const [loading, isLoading] = useState(false);
+  const router = useRouter();
 
   const onSubmit = (values: any) => {
-    
     isLoading(true);
     axios
       .post("/api/send", values)
       .then((response) => {
-        toast.success(response.data);
+        toast.success("Questionnaire sent successful!");
       })
       .catch((error) => {
-        console.log(error);
+        console.log("Questionaire was not sent!");
       })
       .finally(() => {
         isLoading(false);
+        router.push("/");
       });
   };
 
   return (
     <div className="pt-20 md:pt-0">
-      <Hero image={aboutImage} title={""} description={""} />      
-        <VideoForm onSubmit={onSubmit} disabled={loading}/>    
-      
+      <Hero image={aboutImage} title={""} description={""} />
+      <VideoForm onSubmit={onSubmit} disabled={loading} />
     </div>
   );
 };
--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,38 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Prisma } from "@prisma/client";
 import prisma from "@/prisma/prisma";
-
+import {format} from 'date-fns'
 import {Resend} from 'resend'
 import { EmailTemplate } from "@/app/(utils)/video-questionnaire/components/emailTemplate";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const POST = async (request: NextRequest) =>{
     const body = await request.json();
-    try{
+    console.log(new Date(body.weddingDate).toISOString())
+    
         const newQuestionnaire  = await prisma.video.create({
             data:{
                 brideName: body.brideName,
                 groomName: body.groomName,
                 email: body.email,
-                weddingDate: body.weddingDate.toISOString(),
+                weddingDate: new Date(body.weddingDate).toISOString(),
                 songSelection: body.songsOptions.value,
                 highlightSong: body?.highlightSong,
                 videoSongs: body?.videoSongs,
                 details: body?.details,
                 address: body?.address,
                 city: body?.city,
-                state: body?.state,
+                state: body?.state.label,
                 zipCode: body?.zipCode
             }
         })
-
+       
         try{
             const { data, error } = await resend.emails.send({
                 from: 'Video Questionnaire <office@redbarnweddingstudio.xyz>',
-                to: ['rusu55@yahoo.com', 'redbarnbride@yahoo.com'],
-                subject: `Video Questionnaire - ${body.data.brideName} , ${body.data.groomName} - ${body.data.weddingDate}`,
+                to: ['rusu55@yahoo.com'],
+                subject: `Video Questionnaire - ${body.brideName} , ${body.groomName} - ${body.weddingDate}`,
                 react: EmailTemplate({
-                    
+                    brideName: body.brideName,
+                    groomName: body.groomName,
+                    weddingDate: body.weddingDate,
+                    songsOptions: body.songsOptions.value,
+                    highlightSong: body?.highlightSong,
+                    videoSongs: body?.videoSongs,
+                    details: body?.details,
+                    address: body?.address,
+                    city: body?.city,
+                    zipCode: body?.zipCode,
+                    state: body?.state.label
                 })  
             });
             if (error) {
@@ -44,9 +55,4 @@ export const POST = async (request: NextRequest) =>{
             return NextResponse.json({ error }, { status: 500 });
         }        
     }
-    catch(error){
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            throw error
-        }
-    }
-}
+    
