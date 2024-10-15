d02e1c142320fa14adccb367310c9b19c7a07b34
rusu55 <rusu55@outlook.com>
2024-10-14 20:20:43 -0500

    updared

--- app/test/components/HomeCarousel.tsx
+++ app/test/components/HomeCarousel.tsx
@@ -1,9 +1,8 @@
-'use client';
+"use client";
 import Image from "next/image";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-
 
 import { images } from "@/lib/carousel";
 
@@ -15,8 +14,22 @@ export const HomeCarousel = () => {
   return (
     <div className="overflow-hidden">
       <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
+        spaceBetween={0}
+        //centeredSlides={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+          },
+        }}
         loop={true}
         autoplay={{
           delay: 3500,
@@ -31,21 +44,21 @@ export const HomeCarousel = () => {
         {images.map((image, index) => (
           <SwiperSlide key={index}>
             <Image
-             // height={600}
-             // width={1200}
+              // height={600}
+              // width={1200}
               src={image.src}
               alt="Chicago Wedding Photographer"
               fill
-              sizes="(max-width: 600px) 70vw, (max-width: 1920px) 80vw"
+              sizes="(max-width: 600px) 50vw, (max-width: 1920px) 80vw"
               //style={{objectFit: "cover"}}
               className="object-cover object-center"
               //priority={true}
               //loading="eager"
-             //sizes=" 300px, (min-width: 640px) 100vw"
+              //sizes=" 300px, (min-width: 640px) 100vw"
             />
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
-  )
-}
+  );
+};
