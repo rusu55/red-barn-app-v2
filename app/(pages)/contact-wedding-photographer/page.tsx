c7b4dfcd0849a2836e5cf342cef1e6cbb524deac
rusu55 <rusu55@yahoo.com>
2025-01-20 12:54:12 -0600

    updated

--- app/(pages)/contact-wedding-photographer/page.tsx
+++ app/(pages)/contact-wedding-photographer/page.tsx
@@ -5,34 +5,48 @@ import { ContactForm } from "@/components/contact/ContactForm";
 import { ContactIntro } from "@/components/contact/ContactIntro";
 import Img from '/public/images/contact-hero.webp';
 
-
 export const metadata: Metadata = {
-  title: " Wedding Photographer and Videographer Chicago land -  Red Barn Wedding Studio Contact",
-  description: "Top Chicago Wedding Photographer and Videographer  in Chicago land. Wedding Photography and Cinematography at the art level."
-}
+  metadataBase: new URL('https://www.redbarnweddingstudio.com'),
+  title: "Chicago Wedding Photographer - Red Barn Wedding Studio",
+  description:
+    "Your Perfect Wedding with our Chicago Wedding Photographer from Red Barn Wedding Studio, the best of wedding photographers and cinematographers in Chicago",
 
-const Contact = () => {
-  return (
-    <>    
+  openGraph: {
+    title: "Chicago Wedding Photographer - Red Barn Wedding Studio",
+    description:
+      "Your Perfect Wedding with our Chicago Wedding Photographer from Red Barn Wedding Studio, the best of wedding photographers and cinematographers in Chicago",
+    url: "https://redbarnweddingstudio.com",
+    siteName: "Red Barn Wedding Studio",
+    images: [
+      {
+        url: "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/796573d8-903d-4666-ae15-7c39d499072a.jpg",
+        width: 800,
+        height: 600,
+        alt: "Wedding Photographer Chicago",
+      },
+      {
+        url: "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/67a5370f-501a-4ad2-aab2-39169557dcb3.jpg",
+        width: 800,
+        height: 600,
+        alt: "Wedding Photographer Chicago",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Article",
-              "headline": "Wedding Photographer and Videographer Chicago land -  Red Barn Wedding Studio Contact",
-              "description": "Top Chicago Wedding Photographer and Videographer  in Chicago land. Wedding Photography and Cinematography at the art level.",
-              "image": [
+const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebPage',
+    "headline": "Wedding Photographer and Videographer Chicago land -  Red Barn Wedding Studio Contact",
+    "description": "Top Chicago Wedding Photographer and Videographer  in Chicago land. Wedding Photography and Cinematography at the art level.",
+    "image": [
                 "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/796573d8-903d-4666-ae15-7c39d499072a.jpg",
                 "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/67a5370f-501a-4ad2-aab2-39169557dcb3.jpg",
                 "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/67a5370f-501a-4ad2-aab2-39169557dcb3.jpg"
               ],
-              "author": {
-                "@type": "Company",
-                "name": "Red Barn Wedding Studio"
-              },
-              "publisher": {
+    "publisher": {
                 "@type": "Organization",
                 "name": "Red Barn Wedding Studio",
                 "logo": {
@@ -40,13 +54,18 @@ const Contact = () => {
                   "url": "hhttps://redbarnweddingstudio.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.64015fa7.jpg&w=256&q=75"
                 }
               },
-              "mainEntityOfPage": {
+   "mainEntityOfPage": {
                 "@type": "WebPage",
                 "@id": "https://redbarnweddingstudio.com"
-              }
-            }),
-          }}
-        />
+             }
+  }
+const Contact = () => {
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+        
    
     <div className="pt-20 md:pt-0">
       <Hero image={Img} title="Wedding Photographers and Videographers" description={""} />
@@ -59,7 +78,7 @@ const Contact = () => {
         </div>
       </div>
     </div>
-    </>
+   </>
   );
 };
 
