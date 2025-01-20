8df5ea37dc204baa9837b2e10bb38b7f18594fd5
rusu55 <rusu55@yahoo.com>
2025-01-20 13:18:00 -0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,15 @@ import { HomeIcons } from "@/components/home/HomeIcons";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://www.redbarnweddingstudio.com'),
+  metadataBase: new URL('https://redbarnweddingstudio.com'),
   title: "Chicago Wedding Photographer - Red Barn Wedding Studio",
   description:
-    "Your Perfect Wedding with our Chicago Wedding Photographer from Red Barn Wedding Studio, the best of wedding photographers and cinematographers in Chicago",
+    "Perfect Wedding with Chicago Wedding Photographer from Red Barn Wedding Studio - the best wedding photographers and cinematographers in Chicago",
 
   openGraph: {
     title: "Chicago Wedding Photographer - Red Barn Wedding Studio",
     description:
-      "Your Perfect Wedding with our Chicago Wedding Photographer from Red Barn Wedding Studio, the best of wedding photographers and cinematographers in Chicago",
+      "Perfect Wedding with Chicago Wedding Photographer from Red Barn Wedding Studio - the best wedding photographers and cinematographers in Chicago",
     url: "https://redbarnweddingstudio.com",
     siteName: "Red Barn Wedding Studio",
     images: [
@@ -37,6 +37,30 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
   },
+};
+
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'WebPage',
+  "headline": "Chicago Wedding Photographer - Red Barn Wedding Studio",
+  "description": "Perfect Wedding with Chicago Wedding Photographer from Red Barn Wedding Studio - the best wedding photographers and cinematographers in Chicago",
+  "image": [
+              "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/796573d8-903d-4666-ae15-7c39d499072a.jpg",
+              "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/67a5370f-501a-4ad2-aab2-39169557dcb3.jpg",
+              "https://files.edgestore.dev/0xr56vabfzzugegm/publicFiles/_public/331cb3dd-496f-4e0b-914d-c49274aef17e.jpg"
+            ],
+  "publisher": {
+              "@type": "Organization",
+              "name": "Red Barn Wedding Studio",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "hhttps://redbarnweddingstudio.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.64015fa7.jpg&w=256&q=75"
+              }
+            },
+ "mainEntityOfPage": {
+              "@type": "WebPage",
+              "@id": "https://redbarnweddingstudio.com"
+           }
 };
 const Home = async () => {
   const highlights = await prisma.blog.findMany({
