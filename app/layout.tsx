3ecaf8342579fa3a1f6280ffbdeca39061433820
rusu55 <rusu55@yahoo.com>
2025-02-25 12:38:05 -0600

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,11 +33,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
       <head>
           <Script  defer src="https://cloud.umami.is/script.js" data-website-id="bbd44199-17b1-465c-a696-43d2f251e3dd" />
       </head>
       <body className={`${nunito.variable} ${old_standard.variable}`}>
+     
+          <Script id="fb-pixel">
+              {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '518469474692190');
+              fbq('track', 'PageView');
+              `}
+          </Script>
+          <noscript><img height="1" width="1" style={{display: 'none'}}
+          src={`https://www.facebook.com/tr?id=518469474692190&ev=PageView&noscript=1`}
+          /></noscript>
+          {/* End Meta Pixel Code */}
         <NavBar />
         <ToasterProvider />
         <ParallaxProviders>{children}</ParallaxProviders>
